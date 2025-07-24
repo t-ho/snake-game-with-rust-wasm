5d8a04b0246e216d626d664b11c2f85d99baffc1
t-ho <t.ho@tdev.app>
2025-07-23 19:15:28 -0700

    Be able to move the snake

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,130 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Clone)]
+pub struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+    direction: Direction,
+}
+
+impl Snake {
+    fn new(spawn_index: usize, size: usize) -> Self {
+        let mut body = vec![];
+
+        for i in 0..size {
+            body.push(SnakeCell(spawn_index - i));
+        }
+
+        Self {
+            body,
+            direction: Direction::Right, // Default direction,
+        }
+    }
+}
+
+#[wasm_bindgen]
 pub struct Jungle {
     width: usize,
+    size: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl Jungle {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> Self {
-        Self::default()
+    pub fn new(width: usize, snake_idx: usize) -> Self {
+        Self {
+            width,
+            size: width * width,
+            snake: Snake::new(snake_idx, 3),
+        }
     }
 
-    #[wasm_bindgen(getter)]
     pub fn width(&self) -> usize {
         self.width
     }
-}
 
-impl Default for Jungle {
-    fn default() -> Self {
-        Self { width: 20 }
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+
+    // *const is raw pointer
+    // borrowing rules do not apply to it
+    pub fn snake_cells_ptr(&self) -> *const SnakeCell {
+        self.snake.body.as_ptr()
+    }
+
+    // Cannot return a reference to a vector to JavaScript because of borrowing rules.
+    // pub fn snake_cells(&self) -> &Vec<SnakeCell> {
+    //     &self.snake.body
+    // }
+
+    pub fn snake_length(&self) -> usize {
+        self.snake.body.len()
+    }
+
+    pub fn step(&mut self) {
+        let temp = self.snake.body.clone();
+        let next_cell = self.gen_next_snake_cell();
+        self.snake.body[0] = next_cell;
+
+        let len = self.snake.body.len();
+
+        for i in 1..len {
+            self.snake.body[i] = SnakeCell(temp[i - 1].0);
+        }
+    }
+
+    fn gen_next_snake_cell(&self) -> SnakeCell {
+        let snake_head = self.snake_head_idx();
+        let row = snake_head / self.width;
+
+        match self.snake.direction {
+            Direction::Up => {
+                let threshold = snake_head - row * self.width;
+                if snake_head == threshold {
+                    SnakeCell(self.size - self.width + threshold)
+                } else {
+                    SnakeCell(snake_head - self.width)
+                }
+            }
+            Direction::Down => {
+                let threshold = snake_head + ((self.width - row) * self.width);
+                if snake_head + self.width == threshold {
+                    SnakeCell(threshold - ((row + 1) * self.width))
+                } else {
+                    SnakeCell(snake_head + self.width)
+                }
+            }
+
+            Direction::Left => {
+                let threshold = row * self.width;
+                if snake_head == threshold {
+                    SnakeCell(threshold + (self.width - 1))
+                } else {
+                    SnakeCell(snake_head - 1)
+                }
+            }
+            Direction::Right => {
+                let threshold = (row + 1) * self.width;
+                if snake_head + 1 >= threshold {
+                    SnakeCell(threshold - self.width)
+                } else {
+                    SnakeCell(snake_head + 1)
+                }
+            }
+        }
+    }
+
+    pub fn change_snake_direction(&mut self, direction: Direction) {
+        self.snake.direction = direction;
     }
 }
--- www/src/main.ts
+++ www/src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
-import init, { Jungle } from "../pkg/snake_game_with_rust_wasm";
+import init, { Jungle, Direction } from "../pkg/snake_game_with_rust_wasm";
+import type { InitOutput } from "../pkg/snake_game_with_rust_wasm";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -9,12 +10,15 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 `;
 
 const CELL_SIZE = 20;
+const JUNGLE_WIDTH = 8;
+
+const snakeSpawnIdx = Date.now() % (JUNGLE_WIDTH * JUNGLE_WIDTH);
 
 async function start() {
-  await init();
+  const wasm = await init();
 
-  const jungle = new Jungle();
-  const jungleWidth = jungle.width;
+  const jungle = Jungle.new(JUNGLE_WIDTH, snakeSpawnIdx);
+  const jungleWidth = jungle.width();
 
   const canvas = document.getElementById("jungle-canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d")!;
@@ -22,23 +26,91 @@ async function start() {
   canvas.width = jungleWidth * CELL_SIZE;
   canvas.height = jungleWidth * CELL_SIZE;
 
-  drawGrid(ctx, jungleWidth);
-}
+  document.addEventListener("keydown", (event) => {
+    switch (event.code) {
+      case "ArrowUp": {
+        jungle.change_snake_direction(Direction.Up);
+        break;
+      }
+      case "ArrowRight": {
+        jungle.change_snake_direction(Direction.Right);
+        break;
+      }
+      case "ArrowDown": {
+        jungle.change_snake_direction(Direction.Down);
+        break;
+      }
+      case "ArrowLeft": {
+        jungle.change_snake_direction(Direction.Left);
+        break;
+      }
+      default:
+    }
+  });
 
-function drawGrid(ctx: CanvasRenderingContext2D, width: number) {
-  ctx.beginPath();
-
-  for (let x = 0; x <= width; x++) {
-    ctx.moveTo(x * CELL_SIZE, 0);
-    ctx.lineTo(x * CELL_SIZE, width * CELL_SIZE);
+  function update(ctx: CanvasRenderingContext2D, jungle: Jungle) {
+    const fps = 5;
+    setTimeout(() => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      jungle.step();
+      paint(wasm, ctx, jungle);
+      requestAnimationFrame(() => update(ctx, jungle));
+    }, 1000 / fps);
   }
 
-  for (let y = 0; y <= width; y++) {
+  paint(wasm, ctx, jungle);
+  update(ctx, jungle);
+}
+
+function drawJungle(ctx: CanvasRenderingContext2D, jungle: Jungle) {
+  ctx.beginPath();
+  const jungleWidth = jungle.width();
+
+  for (let x = 0; x <= jungleWidth; x++) {
+    ctx.moveTo(x * CELL_SIZE, 0);
+    ctx.lineTo(x * CELL_SIZE, jungleWidth * CELL_SIZE);
+  }
+
+  for (let y = 0; y <= jungleWidth; y++) {
     ctx.moveTo(0, y * CELL_SIZE);
-    ctx.lineTo(width * CELL_SIZE, y * CELL_SIZE);
+    ctx.lineTo(jungleWidth * CELL_SIZE, y * CELL_SIZE);
   }
 
   ctx.stroke();
+}
+
+function drawSnake(
+  wasm: InitOutput,
+  ctx: CanvasRenderingContext2D,
+  jungle: Jungle,
+) {
+  const jungleWidth = jungle.width();
+  const snakeCellPtr = jungle.snake_cells_ptr();
+  const snakeLength = jungle.snake_length();
+  const snakeCells = new Uint32Array(
+    wasm.memory.buffer,
+    snakeCellPtr,
+    snakeLength,
+  );
+
+  ctx.beginPath();
+  snakeCells.forEach((cellIdx, i) => {
+    const col = cellIdx % jungleWidth;
+    const row = Math.floor(cellIdx / jungleWidth);
+
+    ctx.fillStyle = i === 0 ? "#7878db" : "#000";
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  });
+  ctx.stroke();
+}
+
+function paint(
+  wasm: InitOutput,
+  ctx: CanvasRenderingContext2D,
+  jungle: Jungle,
+) {
+  drawJungle(ctx, jungle);
+  drawSnake(wasm, ctx, jungle);
 }
 
 start().catch(console.error);
